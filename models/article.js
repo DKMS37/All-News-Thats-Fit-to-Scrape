d500d00bf8504776e24ab1e4e4760bdbe7cd48ea
McBk_Pro_Engine <dkria@dkrias-MacBook-Pro.local>
2019-01-26 19:49:30 -0800

    making the front end

--- models/article.js
+++ models/article.js
@@ -1,33 +1,40 @@
+var mongoose = require("mongoose");
+
+// save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+// create a new article schema. The link should be unique, but the other properties are not required because they may not exist on the website to be scraped. There is validation on the route to add them to the database on if these properties exist.
 var ArticleSchema = new Schema({
-    // 'title' is required and of type String
-    title: {
-        type: String,
-        require: true,
-        unique: true
-    },
-    // 'link' is required and of type String
-    link: {
-        type: String,
-        unique: true,
-        require: true
-    },
-    intro: {
-        type: String,
-        require: false
-    },
-    saved: {
-        type: Boolean,
-        default: false
-    },
-    // 'note' is an object that stores a Note id 
-    // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with associated Note
-    notes: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Note"
-        }
-    ]
+  title: {
+    type: String,
+    unique: true,
+    require: false
+  },
+  link: {
+    type: String,
+    unique: true,
+    require: false
+  },
+  intro: {
+    type: String,
+    require: false
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+
+  // 'note' is an object that stores a Note id 
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with associated Note
+  notes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    }
+  ]
 });
 
 // create model
